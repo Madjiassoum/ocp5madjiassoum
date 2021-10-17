4256938ce471285c5e4ca503890d7794d9db262c
Madjiassoum <madjiassoum.elysee@hotmail.com>
2021-10-17 21:11:45 +0200

    recherche solution pour http pour heroku

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-let URL_API_PRODUITS = "http://localhost:3000/api/teddies";
+let URL_API_PRODUITS = "c";
 document.addEventListener("DOMContentLoaded", () => {
   const produitsListe = document.getElementById("container-produits");
   if (produitsListe) {
